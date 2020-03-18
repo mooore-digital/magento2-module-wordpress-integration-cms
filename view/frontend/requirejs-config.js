828c70d6dbffd235749d4863bd79ccf64a764f80
Jonathan Visser <jonathan@mooore.nl>
2020-03-18 15:00:16 +0100

    added support for recaptcha

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,6 +2,7 @@ var config = {
     map: {
         '*': {
             'slick': 'Mooore_WordpressIntegrationCms/js/lib/slick',
+            'recaptcha': '//www.google.com/recaptcha/api.js?onload=onCaptchaLoaded&render=explicit'
         }
     }
 };
