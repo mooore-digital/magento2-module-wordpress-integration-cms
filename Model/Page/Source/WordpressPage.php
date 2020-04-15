78170450e0865f846ad094be2f5349992a6e550c
Fabio <fabio@marissen.net>
2020-04-15 15:59:55 +0200

    IMP files after feedback

---
+++ Model/Page/Source/WordpressPage.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mooore\WordpressIntegrationCms\Model\Page\Source;
+
+use Magento\Framework\Data\OptionSourceInterface;
+use Mooore\WordpressIntegrationCms\Model\RemotePageRepository;
+
+class WordpressPage implements OptionSourceInterface
+{
+    /**
+     * @var RemotePageRepository
+     */
+    private $remotePageRepository;
+
+    public function __construct(RemotePageRepository $remotePageRepository)
+    {
+        $this->remotePageRepository = $remotePageRepository;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toOptionArray()
+    {
+        $options = [];
+
+        $options[] = [
+            'label' => __('Select a page...'),
+            'value' => ''
+        ];
+
+        foreach ($this->remotePageRepository->getList() as $site) {
+            foreach ($site['data'] as $page) {
+                $options[] = [
+                    'label' => $page['title']['rendered'] . ' (' . $site['name'] . ')',
+                    'value' => $site['id'] .'_' . $page['id']
+                ];
+            }
+        }
+
+        return $options;
+
+        /*
+         *
+        $configOptions = $this->pageLayoutBuilder->getPageLayoutsConfig()->getOptions();
+        $options = [];
+        foreach ($configOptions as $key => $value) {
+            $options[] = [
+                'label' => $value,
+                'value' => $key,
+            ];
+        }
+        $this->options = $options;
+
+        return $options;
+         */
+    }
+}
