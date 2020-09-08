b5b7092de0cbe992c0ee258700f340f5a7162452
Grimlink <sean.grimlink@gmail.com>
2020-09-08 16:33:00 +0200

    DEL: formidable for plugin

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,9 +3,7 @@ var config = {
         "*": {
             slick: "Mooore_WordpressIntegrationCms/js/lib/slick",
             "kt-slider": "Mooore_WordpressIntegrationCms/js/kadence/kt-slick-init",
-            "kt-tabs": "Mooore_WordpressIntegrationCms/js/kadence/kt-tabs",
-            formidable: "Mooore_WordpressIntegrationCms/js/formidable/index",
-            recaptcha: "https://www.google.com/recaptcha/api.js?onload=onCaptchaLoaded&render=explicit"
+            "kt-tabs": "Mooore_WordpressIntegrationCms/js/kadence/kt-tabs"
         }
     }
 };
