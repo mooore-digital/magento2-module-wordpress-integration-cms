e0240b87ff6daa5df2fc1ce59c550fdd2d7e5039
Jonathan Visser <jonathan@mooore.nl>
2023-02-27 11:05:37 +0100

    IMP: Move blog to addon module

--- Model/HttpClient/Wordpress.php
+++ Model/HttpClient/Wordpress.php
@@ -8,9 +8,10 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
-class Post
+class Wordpress
 {
     const WP_JSON_URL_PREFIX = '/wp-json/wp/v2/';
+    public const WP_JSON_URL_SUFFIX = '';
 
     /**
      * @var \Symfony\Contracts\HttpClient\HttpClientInterface
@@ -63,7 +64,7 @@ class Post
         while ($pageCount < $totalPages) {
             $response = $this->client->request(
                 'GET',
-                self::WP_JSON_URL_PREFIX . 'posts?' . http_build_query([
+                static::WP_JSON_URL_PREFIX . static::WP_JSON_URL_SUFFIX . '?' . http_build_query([
                     'page' => $pageNumber++,
                     'per_page' => $pageSize
                 ])
@@ -89,9 +90,25 @@ class Post
      */
     public function get(int $id): array
     {
-        $response = $this->client->request('GET', self::WP_JSON_URL_PREFIX . 'posts/' . $id);
+        $response = $this->client->request('GET', static::WP_JSON_URL_PREFIX . static::WP_JSON_URL_SUFFIX . '/' . $id);
 
         return json_decode($response->getContent(), true);
+    }
+
+    public function postMetaDataToPage(int $pageId, string $key, string $value, string $authentication)
+    {
+        try {
+            $response = $this->client->request(
+                'POST',
+                static::WP_JSON_URL_PREFIX . static::WP_JSON_URL_SUFFIX . '/' . $pageId . '?'. $key .'=' . $value,
+
+                [
+                    'auth_basic' => $authentication
+                ]
+            );
+        } catch (TransportExceptionInterface $tce) {
+            $this->logger->error('Error: Meta data could not be posted. Reason: ' . $tce->getMessage());
+        }
     }
 
     /**
@@ -106,7 +123,7 @@ class Post
      */
     public function peek(int $pageSize): array
     {
-        $peekResponse = $this->client->request('HEAD', self::WP_JSON_URL_PREFIX . 'posts?per_page=' . $pageSize);
+        $peekResponse = $this->client->request('HEAD', static::WP_JSON_URL_PREFIX . static::WP_JSON_URL_SUFFIX . '?per_page=' . $pageSize);
 
         return $peekResponse->getHeaders();
     }
